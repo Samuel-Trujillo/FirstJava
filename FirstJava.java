8f26df5edc655e4185658ebb3e637f229bc811d5
Samuel-Trujillo <samuel.trujillo093@gmail.com>
2021-06-07 11:26:22 -0700

    commit 1

---
+++ FirstJava.java
@@ -0,0 +1,8 @@
+public class FirstJava{
+    public static void main(String[] args) {
+        System.out.println("My name is Samuel Trujillo");
+        System.out.println("I am 27 years old");
+        System.out.println("My hometown is Riverside, CA");
+        
+    }
+}
